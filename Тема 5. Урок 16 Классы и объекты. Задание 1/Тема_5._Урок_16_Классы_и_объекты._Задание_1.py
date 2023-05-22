3abaa423d6eef9eff923fdfc4f93f4515f76b8cc
79997 <79997@CYBERPC>
2023-05-22 12:39:43 +0400

    Добавьте файлы проекта.

---
+++ Тема 5. Урок 16 Классы и объекты. Задание 1/Тема_5._Урок_16_Классы_и_объекты._Задание_1.py
@@ -0,0 +1,21 @@
+﻿class Cassa:
+
+    summa = 27455 #колличество дененг в кассе
+    
+    def top_up(self, pokup):
+
+        self.pokupka=pokup
+        pokup+=Cassa.summa
+        return f"В кассе {pokup}"
+
+    def count_1000(self):
+        print(Cassa.summa//1000)
+
+    def take_away(self, x):
+        if x <= self.summa:
+            self.summa -= x
+        else:
+            return f"Недостаточно денег"
+
+r=Cassa()
+print(r.take_away(28000))
